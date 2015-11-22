2fd9c9631be5ed65de1b5e32a540e84adae1b677
hbjerry <hbjerryzl@gmail.com>
2015-11-21 23:04:46 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@
 ############################  SETUP PARAMETERS
 app_name='spf13-vim'
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/.spf13-vim-3"
-[ -z "$REPO_URI" ] && REPO_URI='https://github.com/taxilian/spf13-vim.git'
+[ -z "$REPO_URI" ] && REPO_URI='https://github.com/hbjerry/spf13-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0-new'
 debug_mode='0'
 fork_maintainer='0'
